31e6616e317a9eca36a89fa9b65765e6457888a4
Arief Rachman <arief.rachman@ar-raniry.ac.id>
2024-03-18 10:54:09 +0700

    commit buat model Loan

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from  datetime import datetime
 
 # Create your models here.
+class Loan(models.Model):
+    product_name = models.CharField(max_length=55)
+    tenor_pinjaman = models.IntegerField(default=0)
+    tanggal_publish = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(blank=True, null=True)
+    updated_date = models.DateTimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.created_date:
+            self.created_date = datetime.now()
+
+        self.updated_date = datetime.now()
+        super().save(*args, **kwargs)
